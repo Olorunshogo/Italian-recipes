f263e238631487bba204f0510fd737b561f8ab23
BAMTEFA Olorunshogo Moses <shownzy001@gmail.com>
2024-04-20 05:28:42 +0100

    Working on the drop down at the moment.

--- main.js
+++ main.js
@@ -3,3 +3,13 @@ function tada() {
     alert("TADA!!!")
 }
 
+// SIDEMENU
+function openSideMenu() {
+    document.getElementById('side-menu').style.width = '200px';
+}
+openSideMenu()
+
+function closeSideMenu() {
+    document.getElementById('side-menu').style.width = '0';
+    document.getElementById('side-menu').style.width = '0';
+}
