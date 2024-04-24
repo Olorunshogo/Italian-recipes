081f31c86024ef7615038e942ff8bb5f09747b09
BAMTEFA Olorunshogo Moses <shownzy001@gmail.com>
2024-04-24 18:34:37 +0100

    Made a few changes to the sign-up page. Not done yet though.

--- main.js
+++ main.js
@@ -7,9 +7,28 @@ function tada() {
 function openSideMenu() {
     document.getElementById('side-menu').style.width = '200px';
 }
-openSideMenu()
 
 function closeSideMenu() {
     document.getElementById('side-menu').style.width = '0';
     document.getElementById('side-menu').style.width = '0';
 }
+
+    // Read More Buttons
+/*
+function readMore() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("readMore");
+
+    if (moreText.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
+
+*/
