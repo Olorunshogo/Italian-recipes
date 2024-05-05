dc149dd21cfcc20da1ecd360d5076029d68b1da9
BAMTEFA Olorunshogo Moses <shownzy001@gmail.com>
2024-05-05 22:14:13 +0100

    I just need to style the aside part of this Dashboard and make it responsive.

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function author() {
     alert("Author is:  BAMTEFA, Olorunshogo Moses ☺☺☺☺.")
 }
 
-    // SIDEMENU
+            // SIDEMENU
     // Open Button
 {
     const openButton = document.getElementById("open-button");
@@ -27,3 +27,48 @@ function author() {
         document.getElementById('side-menu').style.width = '0';
     });
 }
+
+    // LOGIN Form
+{
+    let loginForm = document.getElementById("loginForm");
+    loginForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        let email = document.getElementById("logEmail");
+        let password = document.getElementById("logPassword");
+
+        if (email.value == "" || password.value == "") {
+            alert("Please ensure you input a value in both fields.")
+        } else {
+            // Perform operation with form input.
+            alert("This form has been submitted, successfully.");
+            console.log(
+                "This form has an Email of " + email.value + " and password of " + password.value + "."
+            );
+
+            email.value = "";
+            password.value = "";
+        }
+    });
+} 
+
+// SIGN UP FORM
+{
+    let signupFOrm = document.getElementById("sign-up-form");
+    signupFOrm.addEventListener("click", function() {
+        document.getElementById("signinForm").submit();
+    });
+}
+
+// FAVORITES at Each Recipe part
+// document.getElementById("heart-icon").addEventListener("load", noSHDisplay());
+// function noSHDisplay() {
+//     document.getElementById("solid-heart").display = "none";
+//     alert("Icon is good");
+// }
+
+// let regularHeart = document.getElementById("regular-heart");
+// regularHeart.addEventListener("click", function() {
+//     document.getElementById("regular-heart").style.display = "none";
+//     document.getElementById("solid-heart").style.display = "block";
+// });
