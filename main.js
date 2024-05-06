830339c924da42d34577c6d390b88745a50c71de
BAMTEFA Olorunshogo Moses <shownzy001@gmail.com>
2024-05-06 02:45:35 +0100

    I just fixed the favorite icon and made it toggle from regular to solid and back. I'm about to increase the chart area and put finishing touvhes on the aside section.

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+
+
+    
+    
     // The button in the footer
 function author() {
     alert("Author is:  BAMTEFA, Olorunshogo Moses ☺☺☺☺.")
@@ -5,70 +9,101 @@ function author() {
 
             // SIDEMENU
     // Open Button
-{
-    const openButton = document.getElementById("open-button");
-    openButton.addEventListener("click", function() {
-        document.getElementById('side-menu').style.width = '200px';
-    });
-}
-
-    // Close Button
-{
-    const closeButton = document.getElementById("close-button");
-    const close1 = document.getElementById("home");
-
-    // Close X icon
-    closeButton.addEventListener("click", function() {
-        document.getElementById('side-menu').style.width = '0';
-    });
-
-    // // Showcase Section
-    close1.addEventListener("click", function() {
-        document.getElementById('side-menu').style.width = '0';
-    });
-}
-
-    // LOGIN Form
-{
-    let loginForm = document.getElementById("loginForm");
-    loginForm.addEventListener("submit", (e) => {
-        e.preventDefault();
-
-        let email = document.getElementById("logEmail");
-        let password = document.getElementById("logPassword");
-
-        if (email.value == "" || password.value == "") {
-            alert("Please ensure you input a value in both fields.")
-        } else {
-            // Perform operation with form input.
-            alert("This form has been submitted, successfully.");
-            console.log(
-                "This form has an Email of " + email.value + " and password of " + password.value + "."
-            );
-
-            email.value = "";
-            password.value = "";
-        }
-    });
-} 
-
-// SIGN UP FORM
-{
-    let signupFOrm = document.getElementById("sign-up-form");
-    signupFOrm.addEventListener("click", function() {
-        document.getElementById("signinForm").submit();
-    });
-}
-
-// FAVORITES at Each Recipe part
-// document.getElementById("heart-icon").addEventListener("load", noSHDisplay());
-// function noSHDisplay() {
-//     document.getElementById("solid-heart").display = "none";
-//     alert("Icon is good");
+// {
+//     const openButton = document.getElementById("open-button");
+//     openButton.addEventListener("click", function() {
+//         document.getElementById('side-menu').style.width = '200px';
+//     });
 // }
 
-// let regularHeart = document.getElementById("regular-heart");
-// regularHeart.addEventListener("click", function() {
-//     document.getElementById("regular-heart").style.display = "none";
-//     document.getElementById("solid-heart").style.display = "block";
-// });
+    // Close Button
+// {
+//     const closeButton = document.getElementById("close-button");
+//     const close1 = document.getElementById("home");
+
+//         // Close X icon
+//     closeButton.addEventListener("click", function() {
+//         document.getElementById('side-menu').style.width = '0';
+//     });
+
+//     // Showcase Section
+//     close1.addEventListener("click", function() {
+//         document.getElementById('side-menu').style.width = '0';
+//     });
+// }
+
+    // LOGIN Form
+// {
+//     let loginForm = document.getElementById("loginForm");
+//     loginForm.addEventListener("submit", (e) => {
+//         e.preventDefault();
+
+//         let email = document.getElementById("logEmail");
+//         let password = document.getElementById("logPassword");
+
+//         if (email.value == "" || password.value == "") {
+//             alert("Please ensure you input a value in both fields.")
+//         } else {
+//             // Perform operation with form input.
+//             alert("This form has been submitted, successfully.");
+//             console.log(
+//                 "This form has an Email of " + email.value + " and password of " + password.value + "."
+//             );
+
+//             email.value = "";
+//             password.value = "";
+//         }
+//     });
+// } 
+
+// SIGN UP FORM
+// {
+//     let signupFOrm = document.getElementById("sign-up-form");
+//     signupFOrm.addEventListener("click", function() {
+//         document.getElementById("signinForm").submit();
+//     });
+// }
+
+// FAVORITES at Each Recipe part
+function myFunction(x) {
+    x.classList.toggle("fa-solid");
+}
+
+
+// REPORT DATE
+function currentDate() {
+    let dateObj = new Date();
+    const month   = dateObj.getUTCMonth() + 1; // months from 1-12
+    // const day     = dateObj.getUTCDate();
+    const year    = dateObj.getUTCFullYear();
+    const dateDisplayed = month + "/" + year;
+    document.getElementById("currentDate").innerHTML = dateDisplayed;
+}
+currentDate();
+
+// PIE CHART
+{
+    let xValues = ["Calories", "Fats", "Carbohydrate", "Protein"];
+    let yValues = [48, 22, 48, 18];
+    let barColours = [
+        "#e8c3b9", "#00aba9",
+        "#2b5797", "#b91d47"
+    ];
+
+    new Chart ("reportChart", {
+        type: "doughnut",
+        data: {
+            labels: xValues,
+            datasets: [{
+                backgroundColor: barColours,
+                data: yValues
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: "World Wide Wine Production 2018"
+            }
+        }
+    });
+}
