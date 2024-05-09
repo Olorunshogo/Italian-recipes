c68378a89ee7401d8a6f21ed136f09a84fbcfa1f
BAMTEFA Olorunshogo Moses <shownzy001@gmail.com>
2024-05-09 21:32:55 +0100

    So far, I'm at 1000px in my attempt to make the dashboard responsive.

--- main.js
+++ main.js
@@ -64,8 +64,27 @@ function author() {
 //     });
 // }
 
-// FAVORITES at Each Recipe part
-function myFunction(x) {
+        // DASHBOARD
+// SIDEBAR
+const body = document.querySelector("body"),
+      sidebar = body.querySelector(".sidebar"),
+      searchBtn = body.querySelector(".search"),
+      modeSwitch = body.querySelector(".toggle-switch"),
+      modeText = body.querySelector("darkmode-text"),
+      hamburger = body.querySelector(".hamburger");
+
+// Add the "close" class to sidebar
+hamburger.addEventListener("click", function() {
+  sidebar.classList.toggle("close");
+});
+
+// Onclick toggle to the and light mode
+modeSwitch.addEventListener("click", () => {
+  body.classList.toggle("dark");
+});
+
+// FAVORITES at each Recipe part
+function favorite(x) {
     x.classList.toggle("fa-solid");
 }
 
@@ -78,17 +97,48 @@ function currentDate() {
     const year    = dateObj.getUTCFullYear();
     const dateDisplayed = month + "/" + year;
     document.getElementById("currentDate").innerHTML = dateDisplayed;
+    document.getElementById("currentDate").style.color = "#777";
+    document.getElementById("currentDate").style.fontSize = "12px";
 }
 currentDate();
 
-// PIE CHART
+// PIE CHART for report of calories per week based on recipes made.
 {
-    let xValues = ["Calories", "Fats", "Carbohydrate", "Protein"];
-    let yValues = [48, 22, 48, 18];
+    let dateObj = new Date();
+    const currentMonth   = dateObj.getUTCMonth();
+    let month = "";
+    let xValues = ["Calories", "Carbohydrate", "Fats", "Protein"];
+    let yValues = [48, 48, 22, 18];
     let barColours = [
-        "#e8c3b9", "#00aba9",
-        "#2b5797", "#b91d47"
+        "#e8c3b9", "#2b5797",
+        "#00aba9", "#b91d47"
     ];
+
+    if (currentMonth == 0) {
+        month = "January";
+      } else if (currentMonth == 1) {
+        month = "February";
+      } else if (currentMonth == 2) {
+        month = "March";
+      } else if (currentMonth == 3) {
+        month = "April";
+      } else if (currentMonth == 4) {
+        month = "May";
+      } else if (currentMonth == 5) {
+        month = "June";
+      } else if (currentMonth == 6) {
+        month = "July";
+      } else if (currentMonth == 7) {
+        month = "August";
+      } else if (currentMonth == 8) {
+        month = "September";
+      } else if (currentMonth == 9) {
+        month = "October";
+      } else if (currentMonth == 10) {
+        month = "November";
+      } else {
+        month = "December";
+      }
 
     new Chart ("reportChart", {
         type: "doughnut",
@@ -102,7 +152,7 @@ currentDate();
         options: {
             title: {
                 display: true,
-                text: "World Wide Wine Production 2018"
+                text: "Details for the month of " + month + "."
             }
         }
     });
